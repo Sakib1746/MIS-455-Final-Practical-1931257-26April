41df5f896492dc7f2e8a4eef5a5eb9a1e8aaab62
Sakib <sahassrasakib@gmail.com>
2024-04-26 11:34:46 +0600

    Testing API

--- JS/main.js
+++ JS/main.js
@@ -16,3 +16,14 @@ function showMore() {
   document.getElementById('mealMore').classList.toggle('hidden');
 }
 
+function searchMeal() {
+  const mealInput = document.getElementById('Search').value;
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+     
+    })
+    .catch((error) => console.error('Error fetching data:', error));
+}
